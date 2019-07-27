e0cbb7087a276a42742a5e527cd64c3d14b9a47a
Clarisse U <clarisseum@gmail.com>
2019-07-27 13:36:06 +0200

    toggle divs

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,19 @@ $(document).ready(function() {
     // $("#primo").hide();
     // $("#segundo").hide();
     // $("#tertio").hide();
-    $("#prem").click(function() {
+    $("#design").click(function() {
         $("img#prem").toggle();
         $("p#prem").toggle();
         $("p#primo").toggle();
 
     });
-    $("#sec").click(function() {
-        $("#segundo").show();
-        $("img#sec").hide();
-        $("p#segundo").show();
-        $("p#sec").show();
+    $("#devs").click(function() {
+        $("img#sec").toggle();
+        $("p#segundo").toggle();
+        $("p#sec").toggle();
     });
 
-
-    $("#tert").click(function() {
+    $("#prodz").click(function() {
         $("p#tert").toggle();
         $("img#tert").toggle();
         $("p#tertio").toggle();
