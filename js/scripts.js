1db849f1c465224ab8b5c0d441019b08460c1cc1
Clarisse U <clarisseum@gmail.com>
2019-07-27 14:45:32 +0200

    hover on portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -18,5 +18,88 @@ $(document).ready(function() {
         $("p#tert").toggle();
         $("img#tert").toggle();
         $("p#tertio").toggle();
-    })
+    });
+
+});
+$(document).ready(function() {
+    $("#un").hide();
+    $("#one").animate({ opacity: 5 });
+    $("#one").hover(function() {
+            $(this).stop().animate({ opacity: 0.4 });
+            $("#un").fadeIn();
+        }),
+        (function() {
+            $(this).stop().animate({ opacity: 1 });
+            $("#un").fadeOut();
+        });
+    $("#deux").hide();
+    $("#two").animate({ opacity: 5 });
+    $("#two").hover(function() {
+            $(this).stop().animate({ opacity: 0.4 });
+            $("#deux").fadeIn();
+        }),
+        (function() {
+            $(this).stop().animate({ opacity: 1 });
+            $("#deux").fadeOut();
+        });
+    $("#trois").hide();
+    $("#three").animate({ opacity: 5 });
+    $("#three").hover(function() {
+            $(this).stop().animate({ opacity: 0.4 });
+            $("#trois").fadeIn();
+        }),
+        (function() {
+            $(this).stop().animate({ opacity: 1 });
+            $("#trois").fadeOut();
+        });
+    $("#quatre").hide();
+    $("#four").animate({ opacity: 5 });
+    $("#four").hover(function() {
+            $(this).stop().animate({ opacity: 0.4 });
+            $("#quatre").fadeIn();
+        }),
+        (function() {
+            $(this).stop().animate({ opacity: 1 });
+            $("#quatre").fadeOut();
+        });
+    $("#cinq").hide();
+    $("#five").animate({ opacity: 5 });
+    $("#five").hover(function() {
+            $(this).stop().animate({ opacity: 0.4 });
+            $("#cinq").fadeIn();
+        }),
+        (function() {
+            $(this).stop().animate({ opacity: 1 });
+            $("#cinq").fadeOut();
+        });
+    $("#sisi").hide();
+    $("#six").animate({ opacity: 5 });
+    $("#six").hover(function() {
+            $(this).stop().animate({ opacity: 0.4 });
+            $("#sisi").fadeIn();
+        }),
+        (function() {
+            $(this).stop().animate({ opacity: 1 });
+            $("#sisi").fadeOut();
+        });
+    $("#sept").hide();
+    $("#seven").animate({ opacity: 5 });
+    $("#seven").hover(function() {
+            $(this).stop().animate({ opacity: 0.4 });
+            $("#sept").fadeIn();
+        }),
+        (function() {
+            $(this).stop().animate({ opacity: 1 });
+            $("#sept").fadeOut();
+        });
+    $("#huit").hide();
+    $("#eight").animate({ opacity: 5 });
+    $("#eight").hover(function() {
+            $(this).stop().animate({ opacity: 0.4 });
+            $("#huit").fadeIn();
+        }),
+        (function() {
+            $(this).stop().animate({ opacity: 1 });
+            $("#huit").fadeOut();
+        });
 });
